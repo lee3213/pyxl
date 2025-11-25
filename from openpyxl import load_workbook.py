f4039b6ffff50c46126baee655d5d8db58061d54
lee3213@gmail.com <lee3213@gmail.com>
2025-11-25 11:22:49 +0900

    modified:   from openpyxl import load_workbook.py 	new file:   openpyxl_worksheet.py

--- from openpyxl import load_workbook.py
+++ from openpyxl import load_workbook.py
@@ -3,45 +3,51 @@ from openpyxl import load_workbook
 
         # data_only=True로 해줘야 수식이 아닌 값으로 받아온다. 
 load_wb = load_workbook("C:/icon_db/202511/entax_splitted_202511.xlsx", data_only=True)
-        # 시트 이름으로 불러오기 
-load_ws = load_wb['Sheet1']
+        # 시트 이름으로 불러
+        #         # 오기 
+ws_list=[]
+ws_list=load_wb.sheetnames
+for a_ws in ws_list:
+       print (a_ws)
+
+
+load_ws_field = load_wb['field']
 
         # 셀 주소로 값 출력
-print(load_ws['B2'].value)
+print(load_ws_field['B2'].value)
 
         # 셀 좌표로 값 출력
-print(load_ws.cell(3, 2).value)
+print(load_ws_field.cell(3, 2).value)
 
 
         # 지정한 셀의 값 출력
 
-get_cells = load_ws['B3' : 'B6']
+get_cells = load_ws_field['B3' : 'B6']
 for row in get_cells:
     for cell in row:
                 print(cell.value)
 
         # 모든 행 단위로 출력
 
-    for row in load_ws.rows:
+for row in load_ws_field.rows:
             print(row)
 
         # 모든 열 단위로 출력
 
-    for column in load_ws.columns:
+for column in load_ws_field.columns:
             print(column)
 
         # 모든 행과 열 출력
 
-    all_values = []
-    for row in load_ws.rows:
+all_values = []
+for row in load_ws_field.rows:
             row_value = []
             for cell in row:
                 row_value.append(cell.value)
             all_values.append(row_value)
-    print(all_values)
+for row in all_values:
+        print(row)
+print(all_values)
 
-    load_ws.cell(3, 3, 51470)
-    load_ws.cell(4, 3, 21470)
-    load_ws.cell(5, 3, 1470)
-    load_ws.cell(6, 3, 6470)
-        #load_wb.save("C:/Users/Administrator/Desktop/기준/프로그래밍/과제대행/주식데이터크롤링/output.xlsx")
+
+        
---
+++ openpyxl_worksheet.py
@@ -0,0 +1,95 @@
+from openpyxl import load_workbook
+from openpyxl import Workbook
+
+
+
+        # data_only=True로 해줘야 수식이 아닌 값으로 받아온다. 
+file_path="C:/icon_db/202511/entax_splitted_202511.xlsx"
+sheet_name="customer_index"
+find_string="자운"
+try:
+    load_wb = load_workbook(file_path, data_only=True)
+        # 시트 이름으로 불러
+        #         # 오기 
+    ws_customer_index=load_wb[sheet_name]
+except FileNotFoundError:
+    print(f"ERror: The File '{file_path}' was not found.")
+except KeyError:
+    print(f"Error The Sheet '{sheet_name}' was not found in worksheet.")
+ws_list=[]
+ws_list=load_wb.sheetnames
+# for a_ws in ws_list:
+#     print (a_ws)
+ws_trade=load_wb['sorted_trade']
+ws_trade_detail=load_wb['sorted_detail']
+trade_col=10 
+# id, trade_type,dateof trade,trade_code,biz_of seller, biz of buyer, product,unit price, count, sum of transaction
+trade_detail_col=11
+
+print("columns",ws_customer_index.columns)
+print ("dimensions:",ws_customer_index.dimensions)
+print("min_column",ws_customer_index.min_column)
+print("min_row",ws_customer_index.min_row )
+print("max_column",ws_customer_index.max_column)
+print("max_row",ws_customer_index.max_row )
+
+
+shop={}
+shop_list=[]
+for customer_range in ws_customer_index.iter_cols(min_col=4,max_col=4,min_row=1,max_row=ws_customer_index.max_row,values_only=True):
+    
+        for i,string in enumerate(customer_range):
+            if find_string in string :
+                col_index=customer_range.index
+                print ("loc of ",find_string,i)
+                row_all=ws_customer_index.iter_rows(min_col=1,max_col=8, min_row=i+1,max_row=i+1,values_only=True)
+                
+                for x,cell in enumerate(row_all):
+                    len_find=len(cell)
+                    print("x=",x,len_find)
+                    shop['trade_start']=cell[5]
+                    shop['trade_end']=cell[6]
+                    shop['shop_info_loc']=cell[7]
+                    shop['shop_name']=cell[3]
+                    shop_list.append(shop)
+                    #for z,r_value in enumerate(cell):
+                    #
+                    #    print( z,r_value)
+
+for a_shop in shop_list:
+    for shop_key,shop_hint in a_shop.items():
+        print(shop_key,shop_hint)
+    trade_start_idx=a_shop['trade_start']
+    trade_end_idx=a_shop['trade_start']
+    for x,trade_range in enumerate(ws_trade.iter_rows(min_col=1,max_col=trade_col,min_row=trade_start_idx,max_row=trade_end_idx,values_only=True)):
+        print("trade_idx=",x, "len=",len(trade_range))
+        for y,a_trade in enumerate(trade_range):
+            print ("y=",y,a_trade)
+
+for x,trade_detail_range in enumerate(ws_trade_detail.iter_rows(min_col=1,max_col=trade_detail_col,min_row=9000,max_row=ws_trade_detail.max_row)):
+
+    
+    if x < 5 :
+        print ("\nx=",x)
+        for y,a_detail in enumerate(trade_detail_range):
+            print(a_detail.value, end=", ")
+        '''print("tr_id=",a_detail[1])
+        print("dateof trade=",a_detail[2])
+        print("product_code:",a_detail[3])
+        print("subsctiption:",a_detail[4])
+        print("standard:",a_detail[5])
+        print("unit:",a_detail[6])
+        print("count:",a_detail[7])
+        print("unit_price:",a_detail[8])
+        print("sum:",a_detail[9])
+        print("tax:",a_detail[10])
+        '''           
+from dataclasses import dataclass
+
+@dataclass
+class Product:
+    weight:int = None
+    price:int = None
+
+apple = Product()
+apple.weight = 10
